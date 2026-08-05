823b8e7fe1deb16746d1ce45bd6c742313a13a41
srislabresearch <srislab.research@gmail.com>
2026-08-05 01:36:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,242 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Welcome to MOST 2023</title>
+        <meta name="keywords" content="MOST 2023 conference">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="./main.css">
+		<link rel="stylesheet" href="./font-awesome.min.css">
+
+		
+		<style>
+		    .fa {
+			    padding: 10px;
+				font-size: 30px;
+				width: 50px;
+				text-align: center;
+				text-decoration: none;
+				border-radius: 50%;
+			}
+		
+		    .fa:hover {
+		        opacity: 0.7;
+		    }
+		
+		    .fa-facebook {
+		        background: #3B5998;
+			    color: white;
+			}
+		</style>
+	</head>
+	
+	
+	<body class="right-sidebar is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header" class="wrapper">
+
+					<!-- Logo -->
+						<div id="logo">
+							<h1><a href="http://ieeemobility.org/">IEEE MOST 2023</a></h1>
+							<strong><p>Detroit, MI, May 17-19, 2023</p></strong>
+						</div>
+
+					<!-- Nav -->
+                        <nav id="nav">
+							<ul>
+							    <li style="white-space: nowrap;"><a href="http://ieeemobility.org/">Home</a></li>
+								<li class="current" style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/organization.html">Organization</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/cfp.html">Call for Papers</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/submission.html">Submission</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/program.html">Program</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/workshop.html">Workshop</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/poster.html">Posters/Demos</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/keynote.html">Keynote and Panel</a></li>
+								<li style="white-space: nowrap;"><a href="http://ieeemobility.org/MOST2023/2023/registration.html">Registration</a></li>
+							</ul>
+						</nav>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="wrapper style2">
+				    
+				    <!--<div class="title">MOST 2022</div>-->
+		
+		
+							    
+							    
+					
+					<div class="container">
+						<div class="row gtr-150">
+							<div class="col-8 col-12-small">
+
+								<!-- Content -->
+									<div id="content">
+										<article class="box post">
+                                            <p style="color:black" align="justify">
+											  Mobility shapes our lives. How we get around – be it on foot, by motor vehicle, bicycle, rail, in the air, 
+											  or by water vessel – has become an important discussion not only in the field of engineering and computer science, 
+											  but also in disciplines such as data science, logistics and operation, health, social sciences, and so on. 
+											  New challenges and opportunities arise as the development of connected and autonomous systems meets the demand for 
+											  equitable and accessible mobility solutions from diverse communities. <b>The First IEEE International Conference on Mobility: 
+											  Operations, Services, and Technologies (MOST)</b> is a new conference that focuses on them.
+</p>
+<p style="color:black" align="justify">
+
+											IEEE MOST provides a leading international forum that brings together academic researchers and industrial practitioners 
+											from diverse fields, with the purpose of exploring the advances and sharing future visions in the field of 
+											mobility on land, in the air, and at sea. IEEE MOST 2023 is soliciting high-quality technical papers that focus 
+											on innovative and emerging operations, services, and technologies for both indoor and outdoor mobility, including prototypes, 
+											infrastructure, tools and software systems, data analytics, logistics operations, novel algorithms and applied artificial intelligence, 
+											distributed and connected systems, autonomous vehicles, and so on.
+</p>
+                                            
+											<header class="style1">
+											    <h2 id="important_dates">Important Dates</h2>
+											</header>
+											<table>
+											    <tbody><tr>
+												    <td><b>Abstract Deadline:</b></td>
+                                                    <td><del><strong><font color="green">December 9, 2022</font></strong></del></td>
+                                               
+									
+													
+												</td></tr>
+												
+												
+												 <tr>
+                                                <td>                                    </td>
+                                                <td><strong><font color="green">Extended to December 31, 2022 (firm) </font></strong></td>
+                                                </tr>
+                                                
+												<tr>
+												    <td><b>Paper Deadline:</b>
+													</td>
+													<td><del><strong><font color="green">December 16, 2022</font></strong></del></td>
+                                                    </tr>
+                                                    
+                                                    	 <tr>
+                                                <td>                                    </td>
+                                                <td><strong><font color="green">Extended to December 31, 2022 (firm) </font></strong></td>
+                                                </tr>
+                                                    
+		
+								
+												<tr>
+												    <td><b>Acceptance Notification:</b></td>
+													<td><strong><font color="green">Feburary 17, 2023</font></strong></td>
+												</tr>
+												
+												<tr>
+												    <td><b>Camera Ready:</b></td>
+													<td><strong><font color="green">March 3, 2023</font></strong></td>
+												</tr>
+												
+												
+												<tr>
+												    <td><b>MOST 2023:</b></td>
+													<td><strong><font color="green">May 17-19, 2023</font></strong></td>
+												</tr>
+											</tbody></table>
+                                            
+										</article>
+									</div>
+
+							</div>
+							<div class="col-4 col-12-small">
+
+								<!-- Sidebar -->
+									<div id="sidebar">
+										<section class="box">
+											<header>
+												<h2>Announcements</h2>
+											</header>
+											<ul>
+                                                <li style="color:black"><b>[07/01/2022]</b>&nbsp;&nbsp;<a href="http://ieeemobility.org/MOST2023/2023/cfp.html">Call for papers</a> released</li>
+                                         
+											</ul>
+										</section>
+										
+										
+												<section class="box">
+										<header>
+										    <h2>Join the IEEE MOST Community!</h2>
+										</header>
+										<ul class="style2">
+									
+											
+										
+                                                <li><a href="https://www.linkedin.com/groups/14060518/" target=_blank><img style="padding-top:1px" width="120px" src="MOST_Logo_Final.png" alt="MOST Community"/></a><br />
+                                                
+                                                </center></li>
+
+										</ul>
+										</section>
+										
+										
+										
+										
+										
+									</div>
+							</div>
+						</div>
+					</div>
+					<div class="container"><span id="panels">
+						</span>
+					</div>
+					
+					<div class="container">
+					    <span id="sponsors">
+						    <header class="style1">
+						        <h2>Conference Sponsorship</h2>
+                            </header>
+					        <p></p><center>
+					            <a href="https://www.ieee.org/"><img height="90px" src="./ieee-logo.png" alt="IEEE"/></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					            <a href="https://www.ieee.org/"><img height="90px" src="./STC.png" alt="STC"/></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								</center><p></p>
+								
+						    <header class="style1">
+						        <h2 style="color:#FFD700">Gold Sponsor</h2>
+                            </header>
+					        <p></p><center>
+							    <a href="https://www.thecarlab.org/home"><img height="90px" src="./EmbeddedImage.png" alt="CarLab"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							    <a href="https://techtowndetroit.org/"><img height="90px" src="./techtown logo.png" alt="TechTown"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								</center><p></p>
+								
+								
+								 <header class="style1">
+						        <h2 style="color:#cacaca">Silver Sponsor</h2>
+                            </header>
+					        <p></p><center>
+							    <a href="https://www.pixmoving.com"><img height="100px" src="./PIXMOVING-LOGO-round-white.png" alt="CarLab"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								</center><p></p>
+								
+								
+						</span>
+					</div>
+					
+					<div class="container">
+					<div id="copyright">
+						<ul>
+							<li style="color:black" ;="">MOST 2023: The First IEEE International Conference on Mobility: Operations, Services, and Technologies</li>
+							<li style="color:black" ;="">Design: <a href="http://html5up.net/">HTML5 UP</a></li>
+						</ul>
+					</div>
+					</div>
+				</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="./jquery.min.js.download"></script>
+			<script src="./jquery.dropotron.min.js.download"></script>
+			<script src="./browser.min.js.download"></script>
+			<script src="./breakpoints.min.js.download"></script>
+			<script src="./util.js.download"></script>
+			<script src="./main.js.download"></script>
+
+
+
+	
+<!-- Photo credits:  Ken Lund under CC BY-SA 2.0 (https://creativecommons.org/licenses/by-sa/2.0/)-->
+</body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
